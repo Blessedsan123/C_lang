97d41f97a5bd8d0c0671b0a73f8c6ecaecc4c834
Sandeep Panigrahi <156462298+Blessedsan123@users.noreply.github.com>
2025-07-29 15:46:10 +0530

    Create all_prime_nummbers_in_given_range.c

---
+++ all_prime_nummbers_in_given_range.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int range1;
+    int range2;
+    
+    printf("Enter the range 1 : ");
+    scanf("%d",&range1);
+    printf("Enter the range 2 : ");
+    scanf("%d",&range2);
+    
+    for(int i = range1; i<range2; i++){
+        int count = 0 ; 
+        for(int j = 1; j<=i; j++){
+            if(i%j == 0){
+                count++;
+            }
+        }
+        if(count == 2){
+            printf("%d\n",i);
+        }
+    }
+    return 0;
+}
