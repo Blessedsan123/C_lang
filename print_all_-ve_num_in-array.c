ecbada077140debbf30dbc73eeb925662dc08c21
Sandeep Panigrahi <156462298+Blessedsan123@users.noreply.github.com>
2025-08-02 22:31:38 +0530

    Create print_all_-ve_num_in-array.c

---
+++ print_all_-ve_num_in-array.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5] = {1,-2,4,-5,6};
+    for(int i = 0 ; i<5 ; i++){
+        if(arr[i]<0){
+        printf("%d\n",arr[i]);
+        }
+    }
+
+    return 0;
+}
